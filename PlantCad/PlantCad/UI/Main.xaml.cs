69a4c66262326444ca56e02f80af797cdd0fcc13
foxse <foxse@bk.ru>
2015-11-21 22:56:43 +0300

    2

--- PlantCad/PlantCad/UI/Main.xaml.cs
+++ PlantCad/PlantCad/UI/Main.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PlantCad.UI
 {
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class MainUserControl : UserControl
+    public partial class MainUserControl : Window
     {
         public MainUserControl()
         {
             InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
